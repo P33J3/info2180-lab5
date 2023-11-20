4455812ac166a78a1e573aaff69b8dab654252ea
P33J3 <peterjonlawrence@gmail.com>
2023-11-20 18:47:18 -0500

    Completed Exercise 5

--- world.js
+++ world.js
@@ -1,13 +1,33 @@
 window.onload = () => {
     const result = document.getElementById("result");
 
-    document.getElementById('lookup').addEventListener('click', (event) => {
+    document.getElementById('countrylookup').addEventListener('click', (event) => {
         event.preventDefault();
 
         let country = encodeURIComponent(document.getElementById('country').value.trim());
         console.log('country value', country);
 
         let url = 'http://localhost/info2180-lab5/world.php?country=' + country;
+        console.log('url', url);
+
+        fetch(url)
+            .then(response => {
+                if(response.ok) {
+                    return response.text();
+                }
+            })
+            .then(data => {
+                result.innerHTML = data;
+            })
+    })
+
+    document.getElementById('citylookup').addEventListener('click', (event) => {
+        event.preventDefault();
+
+        let country = encodeURIComponent(document.getElementById('country').value.trim());
+        console.log('country value', country);
+
+        let url = 'http://localhost/info2180-lab5/world.php?country=' + country + '&lookup=cities';
         console.log('url', url);
 
         fetch(url)
--- world.php
+++ world.php
@@ -11,9 +11,21 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$country="";
-$countries=[];
-if($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['country'])) {
+$country= "";
+$lookup = "";
+$countries= [];
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['country']) && isset($_GET['lookup'])) {
+    $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
+    $lookup = filter_input(INPUT_GET, 'lookup', FILTER_SANITIZE_STRING);
+    if (!empty($lookup)) {
+        $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population
+        FROM cities
+        JOIN countries ON cities.country_code = countries.code 
+        WHERE countries.name LIKE '%$country%'");
+        $stmt->execute();
+        $countries = $stmt->fetchAll();
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['country'])) {
     $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
     if (!empty($country)) {
         $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE '%$country%'");
@@ -24,10 +36,54 @@ if($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['country'])) {
 ?>
 
 
-<ul>
-        <?php foreach ($countries as $row): ?>
-            <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-        <?php endforeach; ?>
-</ul>
+<!--<ul>-->
+<!--        --><?php //foreach ($countries as $row): ?>
+<!--            <li>--><?php //= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?><!--</li>-->
+<!---->
+<!--        --><?php //endforeach; ?>
+<!--</ul>-->
+<?php if(isset($_GET['lookup'])): ?>
+<table>
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>District</th>
+        <th>Population</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($countries as $row): ?>
+        <tr>
+            <td><?= $row['name']; ?></td>
+            <td><?= $row['district']; ?></td>
+            <td><?= $row['population']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+    <?php endif; ?>
+
+    <?php if(!isset($_GET['lookup'])): ?>
+</table>
+<table>
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Continent</th>
+        <th>Independence</th>
+        <th>Head of State</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($countries as $row): ?>
+        <tr>
+            <td><?= $row['name']; ?></td>
+            <td><?= $row['continent']; ?></td>
+            <td><?= $row['independence_year']; ?></td>
+            <td><?= $row['head_of_state']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+<?php endif; ?>
 
 
